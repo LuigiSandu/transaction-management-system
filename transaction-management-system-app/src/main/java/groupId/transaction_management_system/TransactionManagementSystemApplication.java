5b546fc1507331eb01d0cb0051954c83854b972c
Luigi <sandu.doru.luigi@gmail.com>
2024-10-10 15:56:51 +0300

    first commit

---
+++ transaction-management-system-app/src/main/java/groupId/transaction_management_system/TransactionManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package groupId.transaction_management_system;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class TransactionManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionManagementSystemApplication.class, args);
+    }
+
+}
