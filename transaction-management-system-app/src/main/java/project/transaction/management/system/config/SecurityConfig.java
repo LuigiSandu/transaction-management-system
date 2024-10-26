31c189aeccd08ac6fbb0caec8cc5aaeaf7f111e1
Luigi <sandu.doru.luigi@gmail.com>
2024-10-26 17:38:34 +0300

    add separate config classes for local db and aws rds db

--- transaction-management-system-app/src/main/java/project/transaction/management/system/config/SecurityConfig.java
+++ transaction-management-system-app/src/main/java/project/transaction/management/system/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())  // Disable CSRF protection for development (new syntax)
+                .csrf(AbstractHttpConfigurer::disable)  // Disable CSRF protection for development (new syntax)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/transaction-management-system/api/v1/users/register").permitAll()  // Allow registration without authentication
                         .anyRequest().authenticated()  // All other requests need authentication
