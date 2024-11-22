33f42223613b0e406bfec5679e79cda14e77cbd1
Luigi <sandu.doru.luigi@gmail.com>
2024-11-22 14:35:01 +0200

    fix docker container jwt error on startup

--- transaction-management-system-app/src/main/java/project/transaction/management/system/config/JWTAuthenticationFilter.java
+++ transaction-management-system-app/src/main/java/project/transaction/management/system/config/JWTAuthenticationFilter.java
@@ -42,6 +42,10 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = getJWTFromRequest(request);
+        if (request.getRequestURI().startsWith("/actuator")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
         if (StringUtils.hasText(token) || ((!request.getRequestURI().contains(LOGIN_PATH)) && (!request.getRequestURI().contains(REGISTER_PATH)))) {
             try {
                 if (tokenGenerator.validateToken(token)) {
